efea8051079b6c53bb8e9ec1ff4b3844c0497386
Charan Tej <charansaripelli@gmail.com>
2026-08-13 15:05:24 +0530

    Serve frontend from Express

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express=require('express');
-const app=express();
-const port=process.env.PORT||8080;
-app.use(express.static("frontend"));
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:`+port);
-}
-)
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "Front-End")));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
