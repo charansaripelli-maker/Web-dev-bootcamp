b117a585e8564b7e7683fca4ad2885708c154e0a
Charan Tej <charansaripelli@gmail.com>
2026-08-13 16:28:38 +0530

    commit3

--- Front-End/index.js
+++ Front-End/index.js
@@ -12,11 +12,7 @@ let users=[
 ]
 let curr=0;
 function toggleuser(){
-    if(curr==0){
-        curr=1;
-    }else{
-        curr=0;
-    }
+  curr=(curr+1)%2;
     let username=document.getElementById("user-name");
     let usergender=document.getElementById("user-gender");
     let userimage=document.getElementById("user-image");
@@ -24,7 +20,4 @@ function toggleuser(){
     username.innerHTML=users[curr].name;
     usergender.innerHTML=users[curr].gender;
     userimage.src=users[curr].image;
-}
-function toggleuser2(){
-  curr=(curr+1)%2;
 }
