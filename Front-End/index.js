6db90f895fa37c9a8e1d35937ea36e8e03422b8c
Charan Tej <charansaripelli@gmail.com>
2026-08-14 10:36:41 +0530

    commit4

--- Front-End/index.js
+++ Front-End/index.js
@@ -21,3 +21,18 @@ function toggleuser(){
     usergender.innerHTML=users[curr].gender;
     userimage.src=users[curr].image;
 }
+function randomuser(){
+fetch("https://randomuser.me/api/")
+.then((response)=>response.json())
+.then((data)=>{
+    let username=document.getElementById("user-name");
+    let usergender=document.getElementById("user-gender");
+    let userimage=document.getElementById("user-image");
+    username.innerHTML=data.results[0].name.first+" "+data.results[0].name.last;
+    usergender.innerHTML=data.results[0].gender;
+    userimage.src=data.results[0].picture.large;
+})
+.catch((error)=>{
+    console.log("Error fetching random user:", error);
+})
+}
